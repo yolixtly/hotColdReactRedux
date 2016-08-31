07e42ef12452a3cd78088621fc1c3d71f15c093c
shay-kennedy <kennedy.rshay@gmail.com>
2016-08-31 09:44:17 -0700

    Working app - document on actions.js

--- dev/js/actions.js
+++ dev/js/actions.js
@@ -1,12 +1,31 @@
-
+/**
+ * { Assigns string to variable to pass into the action.type }
+ * @constant
+ * @type       {string}
+ */
 var GENERATE_NUMBER = 'GENERATE_NUMBER';
+/**
+ * { generateNumber action }
+ *
+ * @return     {Object}  { returns action type which tells reducer to generate new secret number }
+ */
 var generateNumber = function() {
     return {
         type: GENERATE_NUMBER
     };
 };
-
+/**
+ * { Assigns string to variable to pass into the action.type }
+ * @constant
+ * @type       {string}
+ */
 var USER_GUESS = 'USER_GUESS';
+/**
+ * { userGuess action }
+ *
+ * @param      {number}  usersGuess  The users guess
+ * @return     {Object}  { returns action type which tells reducer to compare the userGuess with the secret number }
+ */
 var userGuess = function(usersGuess) {
     return {
         type: USER_GUESS,
